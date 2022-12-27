0e31b7d516b187034528c5b66b9bd8abfd9b28c9
Think201 <think201@Think201---Mac-101.local>
2022-12-27 16:45:09 +0530

    added cors middleware and update routes a little

--- django/authentication/urls.py
+++ django/authentication/urls.py
@@ -3,8 +3,8 @@ from .views import AuthController
 from .views import PublicController
 
 urlpatterns = [
-    path('auth/signup/', AuthController.postSignup),
-    path('auth/signin/', AuthController.postSignin),
-    path('auth/signout/', AuthController.postSignout),
+    path('auth/signup', AuthController.postSignup),
+    path('auth/signin', AuthController.postSignin),
+    path('auth/signout', AuthController.postSignout),
     path('', PublicController.home)
 ]
--- django/backend/settings.py
+++ django/backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     # 'django.contrib.staticfiles',
     'rest_framework',
     'authentication',
-    'django_extensions'
+    'django_extensions',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,10 @@ MIDDLEWARE = [
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
