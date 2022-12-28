97e8c44786773588fd6041222423eac99e32e6c0
Think201 <think201@Think201---Mac-101.local>
2022-12-28 17:14:32 +0530

    authorized dashboard page component - WIP

--- web-app/src/pages/Dashboard.js
+++ web-app/src/pages/Dashboard.js
@@ -1,7 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-export default class Dashboard extends Component {
-  render() {
-    return <div>Dashboard</div>;
-  }
-}
+const Dashboard = () => {
+  const [showDashboard, setShowDashboard] = useState(false);
+  const navigate = useNavigate();
+  useEffect(() => {
+    (async () => {
+      axios({
+        method: "get",
+        url: `${process.env.REACT_APP_API_URL}/auth/profile`,
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      })
+        .then((res) => {
+          setShowDashboard(true);
+        })
+        .catch((err) => {
+          localStorage.removeItem("token");
+          navigate("/signin");
+        });
+    })();
+  }, []);
+
+  return showDashboard && <div>Dashboard</div>;
+};
+
+export default Dashboard;
--- web-app/src/pages/Signin.js
+++ web-app/src/pages/Signin.js
@@ -10,12 +10,14 @@ const Signin = () => {
   const [loading, setLoading] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
   const [_, toastDispatch] = useContext(ToastContext);
+  const navigate = useNavigate();
 
   const handleSubmit = async (values) => {
     setLoading(true);
     await axios
       .post(`${process.env.REACT_APP_API_URL}/auth/signin`, values)
       .then((res) => {
+        console.log(res.data.body.token);
         setLoading(false);
         localStorage.setItem("token", res.data.body.token);
         successToast(toastDispatch, res.data.message);
@@ -31,7 +33,6 @@ const Signin = () => {
       });
   };
 
-  const navigate = useNavigate();
   return (
     <Container>
       <SigninForm
